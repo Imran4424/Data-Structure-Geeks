8f75736276b63609a7a87f82e248588c370461d2
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-22 20:02:27 +0600

    array

--- Array/Rotations/1.Rotation.cpp
+++ Array/Rotations/1.Rotation.cpp
@@ -1,3 +1,18 @@
 /*
 	https://www.geeksforgeeks.org/array-rotation/
+
+	Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the size of the array" << endl;
+
+	int aSize;
+	cin >> aSize;
+
+	return 0;
+}
