42f6170e3d61d695b836ac79073c665129e02b95
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-22 20:32:41 +0600

    array

--- Array/Rotations/1.Rotation.cpp
+++ Array/Rotations/1.Rotation.cpp
@@ -7,10 +7,44 @@
 #include <iostream>
 using namespace std;
 
+void Rotation(int arr[], int aSize)
+{
+	int store = a[0];
+
+	for (int i = 0; i < aSize - 1; ++i)
+	{
+		arr[i] = arr[i+1];
+	}
+
+	arr[aSize - 1] = store;
+}
+
+void RotationHelper(int arr[], int aSize, int nTimes)
+{
+	for (int i = 0; i < nTimes; ++i)
+	{
+		Rotation(arr, aSize);
+	}
+}
+
 void ReadArray(int arr[], int aSize)
 {
 	cout << "enter the " << aSize << " elements of the array" << endl;
 
+	for (int i = 0; i < aSize; ++i)
+	{
+		cin >> arr[i];
+	}
+}
+
+void Display(int arr[], int aSize)
+{
+	for (int i = 0; i < aSize; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
 }
 
 int main(int argc, char const *argv[])
@@ -24,6 +58,16 @@ int main(int argc, char const *argv[])
 
 	ReadArray(int arr, int aSize);
 
+	Display();
+
+	cout << "how many elements" << endl;
+
+	int nTimes;
+	cin >> nTimes;
+
+	RotationHelper(arr, aSize, nTimes);
+
+	Display();
 
 	return 0;
 }
