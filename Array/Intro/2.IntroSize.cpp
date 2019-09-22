7ce18d4c9f26d119776679742495d16bc5fa9759
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-22 19:56:26 +0600

    array

--- Array/Intro/2.IntroSize.cpp
+++ Array/Intro/2.IntroSize.cpp
@@ -10,7 +10,16 @@ int main(int argc, char const *argv[])
 
 	int arr[size];
 
-	
+	cout << "enter the " << size << " elements of the array" << endl;
+
+	for (int i = 0; i < size; ++i)
+	{
+		cin >> arr[i];
+
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
 
 	return 0;
 }
