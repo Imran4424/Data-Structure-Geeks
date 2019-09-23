122395dcabc3852a8ff650d8b19eda83f2824315
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-23 16:14:59 +0600

    array

---
+++ Array/Rotations/1.LeftRotationJuggling.cpp
@@ -0,0 +1,88 @@
+/*
+	https://www.geeksforgeeks.org/array-rotation/
+
+	Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
+*/
+
+#include <iostream>
+using namespace std;
+
+int GCD(int a, int b)
+{
+	if (0 == b)
+	{
+		return a;
+	}
+
+	return GCD(b, a % b); // this one take one more call if a <  b to swap
+
+}
+
+void Rotation(int arr[], int aSize, int nTimes)
+{
+	int storage[nTimes];
+
+	for (int i = 0; i < nTimes; ++i)
+	{
+		storage[i] = arr[i];
+	}
+
+	for (int i = 0; i < aSize - nTimes; ++i)
+	{
+		arr[i] = arr[i + nTimes];
+	}
+
+	int k = 0;
+
+	for (int i = aSize - nTimes; i < aSize; ++i)
+	{
+		arr[i] = storage[k++];
+	}
+
+}
+
+
+void ReadArray(int arr[], int aSize)
+{
+	cout << "enter the " << aSize << " elements of the array" << endl;
+
+	for (int i = 0; i < aSize; ++i)
+	{
+		cin >> arr[i];
+	}
+}
+
+void Display(int arr[], int aSize)
+{
+	for (int i = 0; i < aSize; ++i)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+	cout << "enter the size of the array" << endl;
+
+	int aSize;
+	cin >> aSize;
+
+	int arr[aSize];
+
+	ReadArray(arr, aSize);
+
+	Display(arr, aSize);
+
+	cout << "how many elements" << endl;
+
+	int nTimes;
+	cin >> nTimes;
+
+	Rotation(arr, aSize, nTimes);
+
+	Display(arr, aSize);
+
+	return 0;
+}
